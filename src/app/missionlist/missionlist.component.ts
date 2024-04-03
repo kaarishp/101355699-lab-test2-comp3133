4260b26addd70aa977d371da5154fa4f169f19a0
kaarishp <kingkaarish@gmail.com>
2024-04-03 12:02:46 -0400

    add missionlist component

---
+++ src/app/missionlist/missionlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-missionlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './missionlist.component.html',
+  styleUrl: './missionlist.component.css'
+})
+export class MissionlistComponent {
+
+}
