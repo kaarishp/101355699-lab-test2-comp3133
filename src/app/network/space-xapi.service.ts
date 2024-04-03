626551ed85d6387cd6635e6819196df96709f6ed
kaarishp <kingkaarish@gmail.com>
2024-04-03 12:06:43 -0400

    add api url

--- src/app/network/space-xapi.service.ts
+++ src/app/network/space-xapi.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Mission } from '../model/mission';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpaceXApiService {
+  private apiUrl = 'https://api.spacexdata.com/v3/launches';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getLaunches(): Observable<Mission[]> {
+    return this.http.get<Mission[]>(this.apiUrl);
+  }
 }
